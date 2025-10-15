0fd7beab49aebae50a820ada00bbb98ec7bcc83a
trebolsoftv1-collab <trebolsoftv1@gmail.com>
2025-10-15 13:13:11 -0500

    Update env.py

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -1,7 +1,7 @@
+from __future__ import with_statement
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
-from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
